9ee80b9f97cee9111fcced8ecf0ba3dc239ae61f
Miky9 <mikyf9@windowslive.com>
2017-07-13 13:48:30 +0200

    3

---
+++ BasicSyntax/if-else/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace BasicSyntax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (15 > 5)
+                Console.WriteLine("Pravda");
+            Console.WriteLine("Program zde pokračuje dál");
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ BasicSyntax/if-else/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Obecné informace o sestavení se øídí pøes následující 
+// sadu atributù. Zmìnou hodnot tìchto atributù se upraví informace
+// pøidružené k sestavení.
+[assembly: AssemblyTitle("if-else")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BasicSyntax")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Nastavení ComVisible na false zpùsobí neviditelnost typù v tomto sestavení
+// pro komponenty modelu COM. Pokud potøebujete pøístup k typu v tomto sestavení
+// modelu COM, nastavte atribut ComVisible daného typu na hodnotu True.
+[assembly: ComVisible(false)]
+
+// Následující GUID se používá pro ID knihovny typù, pokud je tento projekt vystavený pro COM.
+[assembly: Guid("5e038ca8-7eee-4a72-a30a-7a2310b2d390")]
+
+// Informace o verzi sestavení se skládá z tìchto ètyø hodnot:
+//
+//      Hlavní verze
+//      Podverze
+//      Èíslo sestavení
+//      Revize
+//
+// Mùžete zadat všechny hodnoty nebo nastavit výchozí èíslo buildu a revize pomocí *, jak je vidìt níže:
+// pomocí zástupného znaku * takto:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
