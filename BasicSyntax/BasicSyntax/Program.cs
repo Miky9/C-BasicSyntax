10545f5b1f0911cb5c5bb99f406b7867d63f8663
M M <mikyf9@windowslive.com>
2017-07-13 12:35:58 +0200

    2.

---
+++ BasicSyntax/BasicSyntax/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+//using system.collections.generic;
+//using system.linq;
+//using system.text;
+//using system.threading.tasks;
+
+namespace BasicSyntax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+            125
+        }
+    }
+}
